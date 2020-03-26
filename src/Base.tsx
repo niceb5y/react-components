97932847cb455b6204da420801a7f81828cc47af
Seungho Kim <niceb5y@gmail.com>
2020-03-26 20:08:32 +0900

    Add Base component

---
+++ src/Base.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Base = () => (
+	<>
+		<link rel="stylesheet" href="https://cdn.shk.im/webcore/v20.css" />
+		<script async src="https://cdn.shk.im/webcore/v20.js"></script>
+	</>
+);
+
+export default Base;
